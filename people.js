4db98c9c5f2ae42a0fe77a6063b3020589983b10
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-07-29 16:09:28 +0200

    [UPD] fine-esercizio

--- people.js
+++ people.js
@@ -1,8 +1,8 @@
 // Importa la tua funzione da names.js Importa la tua funzione da hobbies.js Crea una funzione che non ha parametri. La funzione dovrebbe restituire un oggetto con due proprietà: fullName ed hobbies. All'interno della tua funzione, usa le tue due funzioni precedenti per costruire l'oggetto.
 
 // importo le funzioni
-const names = require("./names.js");
-const hobbies = require("./hobbies.js");
+const names = require("./assets/names");
+const hobbies = require("./assets/hobbies");
 
 const getSubjectInfo = () => {
   return {
