50a6da2ea880e3f376b8345be41f59ea73e2de4f
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-07-29 15:41:04 +0200

    [UPD] step-1

---
+++ assets/names.js
@@ -0,0 +1,10 @@
+// Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName
+
+const user = (firsName, lastName) => {
+  return {
+    firsName,
+    lastName,
+  };
+};
+
+module.exports = user;
